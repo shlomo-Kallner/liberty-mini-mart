5f180381d39ba77660f162dc18b3b1aa22dbe090
Shlomo Kallner <shlomo.kallner@gmail.com>
2018-04-16 22:18:36 +0300

    WRITE: Finished converting bootstrapious/universal to Laravel 5.5.0 prepratory to cutting into components.
    Signed-off-by: Shlomo Kallner <shlomo.kallner@gmail.com>

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,4 +1,19 @@
+<?php
+// setting the version and fileName fields manually for testing purposes only..
+$version = 1;
+$fileName = 'master_';
 
+if ($version === 0) {
+    $fileName .= 'themewagon';
+} elseif ($version == 1) {
+    $fileName .= 'bootstrapious';
+} elseif ($version == 2) {
+    $fileName .= 'startbootstrap';
+} elseif ($version == 3) {
+    $fileName .= 'bootstrapmade';
+}
+//dd($fileName);
+?>
 
-@extends('master_themewagon')
+@extends($fileName)
 
