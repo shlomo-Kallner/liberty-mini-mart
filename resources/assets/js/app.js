cdc348964d75581c91d551ed42a4482a48f180ce
Shlomo Kallner <shlomo.kallner@gmail.com>
2018-11-28 15:47:45 +0200

    update..
    Signed-off-by: Shlomo Kallner <shlomo.kallner@gmail.com>

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -5,6 +5,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 // import { LaravelAlert } from './lib/LaravelAlert'
+require('babel-polyfill');
 window.Laravel.LaravelAlert = require('./lib/LaravelAlert').default;
 
 // import { Pagination } from 'vue-pagination-2'
